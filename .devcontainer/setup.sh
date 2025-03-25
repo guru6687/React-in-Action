3e8af0c66225d7ab56c2d08d455d59b9e30dff43
Guru <gurumishra1@gmail.com>
2025-03-25 02:58:59 +0000

    create a test app

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,3 +1,7 @@
 
 git config --global user.email "gurumishra1@gmail.com"
 git config --global user.name "Guru"
+
+echo "Installing react@18.2.0 react-dom@18.2.0 and install -g npm@11.2.0"
+npm install react@18.2.0 react-dom@18.2.0
+npm install -g npm@11.2.0
